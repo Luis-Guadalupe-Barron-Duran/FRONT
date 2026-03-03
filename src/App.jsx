7b3f780915a764f1ff4f0029cb552ab4a3188de1
Luis-Guadalupe-Barron-Duran <124049120@upq.edu.mx>
2026-03-02 20:22:41 -0600

    Rutas Privadas y publicas (Reorganizadas)

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Layout from './components/layout/Layout';
 import Productos from './pages/Productos';
 import Login from './pages/login';
+import ProtectedRoute from './components/layout/ProtectedRoute';
+
 const Dashboard = () => (
   <div>
     <h1 className="text-3xl font-bold text-slate-800">Dashboard</h1>
@@ -12,24 +14,23 @@ const Dashboard = () => (
 function App() {
   return (
     <BrowserRouter>
-      {/* El Layout envuelve todas las rutas */}
       <Routes>
          <Route path="/login" element={<Login />} />
 
-          {/* Redireccionar raíz a dashboard */}
-          <Route path="/" element={
-          <Navigate to="/dashboard" replace />} />
-          <Route path="/dashboard" element={
+          {/* Privadas */}
+          <Route element={<ProtectedRoute />} >
+          <Route path="/*" element={
             <Layout>
-              <Dashboard />
-              </Layout>} />
-          <Route path="/productos" element={
-            <Layout>
-              <Productos />
-            </Layout>} />
+              <Routes>
+                <Route path="/" element={<Navigate to="/dashboard" replace />} />
+                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/productos" element={<Productos />} />
+              </Routes>
+            </Layout>
+          }/>
+          </Route>
         </Routes>
     </BrowserRouter>
   );
 }
-
 export default App;
